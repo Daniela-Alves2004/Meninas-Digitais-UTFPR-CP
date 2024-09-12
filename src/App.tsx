ff7654e1566ce11c91a34cfa984dc739cb2fec42
Daniela Alves <danieladossantosalves01@gmail.com>
2024-09-12 16:30:12 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="text-center bg-blue-500 text-white p-4">
+      <h1 className="text-4xl font-bold">Hello, Tailwind!</h1>
+    </div>
+  );
+}
+
+
+export default App;
